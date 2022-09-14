34f7996d0de62ce1ab4f61dc44111179da51a596
BertranK <111403561+BertranK@users.noreply.github.com>
2022-09-15 00:14:16 +0900

    a

--- content/javascripts/zfont.js
+++ content/javascripts/zfont.js
@@ -8,9 +8,9 @@ var illo = new Zdog.Illustration({
   dragRotate: true,
   rotate: {x: -0.32, y: 0.64, z: 0},
   resize: 'fullscreen',
-  onResize: function(width) {
-    var minSize = Math.min(width);
-    this.zoom = minSize / 420;
+  onResize: function(width, height) {
+    var minSize = Math.min(width, height);
+    this.zoom = minSize / 200;
   }
 });
 
@@ -27,7 +27,7 @@ var title = new Zdog.TextGroup({
   addTo: illo,
   font: font,
   value: "IronCAD Keyboard Shortcuts Map",
-  fontSize: 20,
+  fontSize: 10,
   translate: {y: -100},
   textAlign: 'center',
   textBaseline: 'middle',
@@ -41,28 +41,6 @@ var titleShadow = title.copyGraph({
   color: '#aab'
 });
 
-// Create a TextGroup object for the subtitle
-// https://github.com/jaames/zfont#zdogtextgroup
-var sub = new Zdog.TextGroup({
-  addTo: illo,
-  font: font,
-  // Pass an array as the text value for multiline text:   
-  value: [
-    'A visual shortcuts explorer for IronCAD and somethings?',
-    '[C] means with Left Click, [D] means with Left Click and drag something'
-  ],
-  fontSize: 12,
-  textAlign: 'center',
-  textBaseline: 'middle',
-  color: '#fff',
-  fill: true,
-});
-
-// Duplicate the subtitle to create a shadow effect
-var subShadow = sub.copyGraph({
-  translate: { z: -6 },
-  color: '#aab'
-})
 
 // Settings for the wave animation
 var t = 0;
